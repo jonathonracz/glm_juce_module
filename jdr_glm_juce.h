ff835b5d08e33404792781e2b226acd26af43dd5
Jonathon Racz <jdracz@umich.edu>
2018-03-24 18:34:34 -0400

    Add GLM submodule + module header

---
+++ jdr_glm_juce.h
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+   Module adaptation Copyright (c) 2018 Jonathon Racz
+   GLM Copyright (c) 2005-2017 G-Truc Creation
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+  ==============================================================================
+*/
+
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               jdr_glm_juce
+  vendor:           Jonathon Racz
+  version:          0.9.9.0
+  name:             jdr_glm_juce
+  description:      Minimal conversion of the GLM library to a JUCE module.
+  website:          https://www.twitter.com/jonathonracz
+  license:          MIT
+  searchpaths:      glm/glm
+
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+#pragma once
+
+#include "glm/glm/glm.hpp"
